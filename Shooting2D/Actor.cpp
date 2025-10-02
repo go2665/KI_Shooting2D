3fd9151571f9238e30c9f37bf4bdcd1cfaaf1c82
go2665 <go2665@outlook.com>
2025-10-02 15:07:53 +0900

    컴포넌트 시스템 추가

--- Shooting2D/Actor.cpp
+++ Shooting2D/Actor.cpp
@@ -32,3 +32,29 @@ void Actor::OnRender(Gdiplus::Graphics* InGraphics)
         static_cast<int>(Position.Y - Size * Pivot.Y),
         Size, Size);  // 그려질 사이즈
 }
+
+void Actor::AddComponent(Component* InComponent)
+{
+    if (InComponent)
+    {
+        Components.push_back(InComponent);
+        InComponent->OnInitialize(); // 컴포넌트 초기화 호출
+    }
+}
+
+void Actor::RemoveComponent(Component* InComponent)
+{
+    if (InComponent)
+    {
+        auto it = std::find(Components.begin(), Components.end(), InComponent);
+        if (it != Components.end())
+        {
+            (*it)->OnDestroy(); // 컴포넌트 파괴 호출
+
+            std::swap(*it, Components.back());
+            Components.pop_back();   // 스왑 앤 팝 방식으로 컴포넌트 제거
+            delete InComponent;
+            InComponent = nullptr;
+        }
+    }
+}
