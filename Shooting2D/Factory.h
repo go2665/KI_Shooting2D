d26993baf0248153676349aff374679e54260ca2
go2665 <go2665@outlook.com>
2025-10-02 10:56:12 +0900

    싱글톤 템플릿 클래스 작성

---
+++ Shooting2D/Factory.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Singleton.h"
+
+class Factory : public Singleton<Factory>
+{
+	friend class Singleton<Factory>;
+
+public:
+private:
+	Factory() = default;
+	virtual ~Factory() = default;
+};
